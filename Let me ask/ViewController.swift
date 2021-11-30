2fdd2ac4f4d6300de9f730a2edc864d90c003849
chumovskih_sergey <sergeyka-lalala@yandex.ru>
2021-11-30 19:58:44 +0300

    Initial Commit

---
+++ Let me ask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Let me ask
+//
+//  Created by Сергей Чумовских  on 30.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
